833f1752f99b9fe7dce2e9bab5b98694aee0d4ce
Marcin Radomski <marcin@mradomski.pl>
2018-06-12 23:00:33 +0200

    asm: handle non-relative address references

--- evil/vm.py
+++ evil/vm.py
@@ -553,12 +553,16 @@ def asm_compile(text: str, char_bit: int) -> typing.List[int]:
 
     for label, refs in label_refs.items():
         for ref, op in refs:
-            rel_addr = labels[label] - ref - Packer.calcsize(op.arg_def)
-            logging.debug('filling address @ %08x with %08x' % (ref, rel_addr))
+            addr = labels[label]
+            needs_relative_addr = op.mnemonic.endswith('.rel')
+            if needs_relative_addr:
+                addr = addr - ref - Packer.calcsize(op.arg_def)
+
+            logging.debug('filling address @ %08x with %08x' % (ref, addr))
             bytecode[ref:ref+Packer.calcsize('a')] = Packer.pack(endianness=op.args_endianness,
                                                                  char_bit=char_bit,
                                                                  fmt='a',
-                                                                 args=[rel_addr])
+                                                                 args=[addr])
 
     return bytecode
 
