a21a926b121d1c6d5e09a4c0981fcdaa15f6f296
Marcin Radomski <marcin@mradomski.pl>
2018-06-14 00:19:24 +0200

    Add hello world example

---
+++ asm/hello.asm
@@ -0,0 +1,15 @@
+    movw.i2r c, hello
+    call.rel print
+    halt
+
+print:
+    lpb.r a, c
+    je.rel print_done
+    out
+    add.b c, 1
+    jmp.rel print
+print_done:
+    ret
+
+hello:
+    db "Hello, world!\0"
